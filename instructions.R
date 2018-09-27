1ecb85f1af1931cf9988dafc66f6c30890498c29
Jared Hembree <hembreeja@appstate.edu>
2018-09-27 15:34:14 -0400

    theming

---
+++ instructions.R
@@ -0,0 +1,5 @@
+install.packages("devtools")
+devtools::install_github('rstudio/blogdown')
+library(blogdown)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+install_hugo()
